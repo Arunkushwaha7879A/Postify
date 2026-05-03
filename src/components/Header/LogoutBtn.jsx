5a676b90e47150f6aa0a67cc240b950e72746ee8
Arun Kushwaha <arun787947@gmail.com>
2026-05-03 23:44:08 +0530

    heading update

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import {useDispatch} from 'react-redux'
-import authService from '../../appwrite/auth'
 import {logout} from '../../store/authSlice'
+import authService from '../../appwrite/auth'
+import {useDispatch} from 'react-redux'
 
+
+//logout buttton
 function LogoutBtn() {
     const dispatch = useDispatch()
     const logoutHandler = () => {
