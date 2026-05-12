21537d4f8a813383571ed21f09ad0da3929dfa30
Arun Kushwaha <arun787947@gmail.com>
2026-05-12 22:39:53 +0530

    updated

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -6,3 +6,13 @@ function Container({children}) {
 }
 
 export default Container
+
+
+
+
+// function Container({children}) {
+//   return <div className='w-full max-w-7xl mx-auto px-4'>{children}</div>;
+  
+// }
+
+// export default Container
